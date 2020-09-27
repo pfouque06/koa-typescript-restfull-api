cd59a6c2c8e77c25ce9b661d15c12a6615d77f52
Philou <pfouque@gmail.com>
2020-09-27 02:18:14 +0200

    add doc

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,11 +1,18 @@
 import 'colors';
 import { Service } from "typedi/decorators/Service";
-import { Connection } from "typeorm";
+import { Connection, Repository } from "typeorm";
 import { User } from "../entities/User";
 import { BaseService } from "./BaseService";
 
 @Service()
 export class UserService extends BaseService<User> {
+
+    // public repo: Repository<User>  --> migrated to BaseService
+
+    // constructor(db: Connection) { 
+    //     this.repo = db.getRepository(User); --> migrated to BaseService
+    // }
+
     constructor(db: Connection) {
         super(db.getRepository(User));
         console.log('Start UserService'.underline);
