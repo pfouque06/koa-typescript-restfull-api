7773f179f69384f7ce48a6b29537411a2bbbd67e
Philou <pfouque@gmail.com>
2020-10-17 10:26:16 +0200

    add details on mock userDataSet

--- src/entities/dataSet/userDataSet.ts
+++ src/entities/dataSet/userDataSet.ts
@@ -19,6 +19,8 @@ export const userDataSet: any[] =
     {
         firstName: 'sam',
         lastName: 'va',
+        birthDate: '01/01/2001',
+        mobile: "0612345678",
         email: 'sam.va@gmail.com',
         password: 'secret',
         profile: 'admin'
@@ -26,6 +28,8 @@ export const userDataSet: any[] =
     {
         firstName: 'jean',
         lastName: 'pile',
+        birthDate: '01/02/2002',
+        mobile: "0687654321",
         email: 'jean.pile@gmail.com',
         password: 'secret',
         profile: 'user'
