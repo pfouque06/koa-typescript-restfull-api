88b0e1add17942a28adc22d47657f72e300826c9
Philou <pfouque@gmail.com>
2020-10-29 01:01:08 +0100

    add more user data set

--- src/entities/dataSet/userDataSet.ts
+++ src/entities/dataSet/userDataSet.ts
@@ -28,9 +28,99 @@ export const userDataSet: any[] =
     {
         firstName: 'jean',
         lastName: 'pile',
-        birthDate: '01/02/2002',
+        birthDate: '02/02/2002',
         mobile: "0687654321",
         email: 'jean.pile@gmail.com',
+        password: 'secret',
+        profile: 'user'
+    },
+    {
+        firstName: 'sam',
+        lastName: 'botte',
+        birthDate: '03/03/2003',
+        mobile: "0687654321",
+        email: 'sam.botte@gmail.com',
+        password: 'secret',
+        profile: 'admin'
+    },
+    {
+        firstName: 'sam',
+        lastName: 'plait',
+        birthDate: '04/04/2004',
+        mobile: "0687654321",
+        email: 'sam.plait@gmail.com',
+        password: 'secret',
+        profile: 'user'
+    },
+    {
+        firstName: 'sam',
+        lastName: 'gratte',
+        birthDate: '05/05/2005',
+        mobile: "0687654321",
+        email: 'sam.gratte@gmail.com',
+        password: 'secret',
+        profile: 'user'
+    },
+    {
+        firstName: 'sam',
+        lastName: 'suffit',
+        birthDate: '06/06/2006',
+        mobile: "0687654321",
+        email: 'sam.suffit@gmail.com',
+        password: 'secret',
+        profile: 'user'
+    },
+    {
+        firstName: 'sam',
+        lastName: 'soule',
+        birthDate: '07/07/2007',
+        mobile: "0687654321",
+        email: 'sam.soule@gmail.com',
+        password: 'secret',
+        profile: 'user'
+    },
+    {
+        firstName: 'jean',
+        lastName: 'barque',
+        birthDate: '08/08/2008',
+        mobile: "0687654321",
+        email: 'jean.barque@gmail.com',
+        password: 'secret',
+        profile: 'user'
+    },
+    {
+        firstName: 'jean',
+        lastName: 'bivalent',
+        birthDate: '09/09/2009',
+        mobile: "0687654321",
+        email: 'jean.bivalent@gmail.com',
+        password: 'secret',
+        profile: 'user'
+    },
+    {
+        firstName: 'jean',
+        lastName: 'darme',
+        birthDate: '10/10/2010',
+        mobile: "0687654321",
+        email: 'jean.darme@gmail.com',
+        password: 'secret',
+        profile: 'user'
+    },
+    {
+        firstName: 'jean',
+        lastName: 'valjean',
+        birthDate: '11/11/2011',
+        mobile: "0687654321",
+        email: 'jean.valjean@gmail.com',
+        password: 'secret',
+        profile: 'user'
+    },
+    {
+        firstName: 'jean',
+        lastName: 'porte',
+        birthDate: '12/12/2012',
+        mobile: "0687654321",
+        email: 'jean.porte@gmail.com',
         password: 'secret',
         profile: 'user'
     }
