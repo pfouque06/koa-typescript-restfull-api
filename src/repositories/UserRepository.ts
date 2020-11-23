35f9b070f19ff7b43683dd1ca790f0999e42da8c
Philou <pfouque@gmail.com>
2020-11-23 02:13:46 +0100

    update docker and userRepo

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -20,14 +20,20 @@ export class UserRepository extends BaseRepository<User> {
 
         console.log(`dropping all tables`.underline);
         // await DBsynchronize(true);
-
+        
         const db = getConnection();
+        console.log(`db type: `,db_type);
         switch (db_type) {
             case "mysql": {
-                await db.query(`SET FOREIGN_KEY_CHECKS=0;`); 
+                console.log(`db query: SET FOREIGN_KEY_CHECKS=0;`);
+                await db.query(`SET FOREIGN_KEY_CHECKS=0;`);
+
                 for await (const entity of userEntityNames) {
+                    console.log(`db query: TRUNCATE TABLE ${db_schema}.${entity};`);
                     const rawData = await db.query(`TRUNCATE TABLE ${db_schema}.${entity};`); 
                 }
+
+                console.log(`db query: SET FOREIGN_KEY_CHECKS=1;`);
                 await db.query(`SET FOREIGN_KEY_CHECKS=1;`); 
                 break;
             }
